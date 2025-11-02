ef8a871f18828013a1d720ddf602549f52a78670
JamesDwyer5 <dwyerj5@mymacewan.ca>
2025-11-02 09:20:30 -0800

    Added customer/employee inserts and phone inserts

--- CMPT291_AS4_X01L-Team3_insert.sql
+++ CMPT291_AS4_X01L-Team3_insert.sql
@@ -1,5 +1,9 @@
 USE Assignment4;
 
+DELETE EmployeePhone;
+DELETE Employee;
+DELETE CustomerPhone;
+DELETE Customer;
 DELETE Movie;
 
 INSERT INTO Movie (MovieID, MovieName, MovieType, Fee, NumOfCopy)
@@ -7,3 +11,17 @@ INSERT INTO Movie (MovieID, MovieName, MovieType, Fee, NumOfCopy)
 
 INSERT INTO Movie (MovieID, MovieName, MovieType, Fee, NumOfCopy)
 	VALUES( NEXT VALUE FOR Movie_MovieID_Seq, 'Top Gun: Maverick', 'Action', 7.0, 3);
+
+
+INSERT INTO Customer(LastName, FirstName, Address, City, State, ZipCode, AccountNumber ,EmailAddress, CreditCardNumber)
+	Values('Smith', 'John', '123456 test St.', 'Edmonton', 'Alberta', 'abc 1b2', NEXT VALUE FOR Customer_AccountNumber_Seq,'test@gmail.com', '1234 5678 91029 1293')
+
+INSERT INTO CustomerPhone(CustomerID, PhoneNum, PhoneType)
+	values( (SELECT CustomerID FROM Customer WHERE AccountNumber = '1000'), '7807654321', 'Home')
+
+INSERT INTO Employee (SSN, LastName, FirstName, Address, City, State, ZipCode, StartDate)
+	VALUES ('123456789', 'Smith', 'John','123456 test St.', 'Edmonton', 'Alberta', 'DEF 1C2', '2024-10-29');
+
+INSERT INTO EmployeePhone (EmployeeID, PhoneNum, PhoneType)
+	VALUES( (SELECT EmployeeID FROM Employee WHERE SSN = '123456789'),
+		'7807635232', 'Home' )
