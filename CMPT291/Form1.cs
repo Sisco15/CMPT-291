f023f3378ad6f158c92cd429d7bc0877d36e3f52
Sisco15 <sisonb@mymacewan.ca>
2025-11-05 19:15:11 -0700

    Login

---
+++ CMPT291/Form1.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Text;
+namespace CMPT291
+{
+    public partial class Form1 : Form
+    {
+        public SqlConnection myConnection;
+        public SqlCommand myCommand;
+        public SqlDataReader myReader;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            String connectionString = "Server = DESKTOP-LGGJQU9; Database = CMPT291_AS2; Trusted_Connection = true;";
+
+            SqlConnection myConnection = new SqlConnection(connectionString); // Timeout in seconds
+
+            try
+            {
+                myConnection.Open(); // Open connection
+                myCommand = new SqlCommand();
+                myCommand.Connection = myConnection; // Link the command stream to the connection
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "Error");
+                this.Close();
+            }
+
+        }
+
+        public string ComputeMd5Hash(string rawData)
+        {
+            using (MD5 md5Hash = MD5.Create())
+            {
+                byte[] bytes = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in bytes)
+                {
+                    builder.Append(b.ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                myCommand.CommandText = "select EmployeeID from user_log where username = '" + textBox1.Text + "' and pass_word = '" + ComputeMd5Hash(textBox2.Text) + "'";
+                MessageBox.Show(myCommand.CommandText);
+
+                using (SqlDataReader reader = myCommand.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        MessageBox.Show("Login Succsess");
+                        
+                        if (reader.Read())
+                        {
+                            int id = reader.GetInt32(reader.GetOrdinal("EmployeeID"));
+                            MessageBox.Show(id.ToString());
+                            this.Hide();
+                            Form2 mainForm = new Form2(id);
+                            mainForm.ShowDialog();
+
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Login Failed");
+                    }
+
+                }
+            }
+            catch (Exception e2)
+            {
+                MessageBox.Show(e2.ToString(), "Error");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CMPT291/Form2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CMPT291
+{
+    public partial class Form2 : Form
+    {
+        private int employeeId;
+        public Form2(int id)
+        {
+            InitializeComponent();
+            employeeId = id;
+        }
+    }
+}
