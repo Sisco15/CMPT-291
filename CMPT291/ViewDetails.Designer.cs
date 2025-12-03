b7fcf3d70062067bf2de9f69c1c7675c5b761dda
zhirep <164239262+zhirep@users.noreply.github.com>
2025-12-02 20:11:26 -0700

    Update ViewDetails.Designer.cs

--- CMPT291/ViewDetails.Designer.cs
+++ CMPT291/ViewDetails.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace CMPT291
+namespace CMPT291
 {
     partial class ViewDetails
     {
@@ -36,23 +36,22 @@
             lbCopy = new Label();
             lbName = new Label();
             lbType = new Label();
-            lbID = new Label();
             dataGridView1 = new DataGridView();
             lbNumOfCpy = new Label();
             lbFee = new Label();
             lbMovieType = new Label();
             lbMovieName = new Label();
-            lbMovieID = new Label();
             lbTitle = new Label();
+            lbRate = new Label();
+            lbRating = new Label();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // refreshButton
             // 
-            refreshButton.Location = new Point(677, 17);
-            refreshButton.Margin = new Padding(4, 5, 4, 5);
+            refreshButton.Location = new Point(474, 10);
             refreshButton.Name = "refreshButton";
-            refreshButton.Size = new Size(107, 38);
+            refreshButton.Size = new Size(75, 23);
             refreshButton.TabIndex = 45;
             refreshButton.Text = "Refresh";
             refreshButton.UseVisualStyleBackColor = true;
@@ -61,19 +60,17 @@
             // lbActors
             // 
             lbActors.AutoSize = true;
-            lbActors.Location = new Point(347, 25);
-            lbActors.Margin = new Padding(4, 0, 4, 0);
+            lbActors.Location = new Point(243, 15);
             lbActors.Name = "lbActors";
-            lbActors.Size = new Size(116, 25);
+            lbActors.Size = new Size(76, 15);
             lbActors.TabIndex = 44;
             lbActors.Text = "List of Actors";
             // 
             // addActor
             // 
-            addActor.Location = new Point(677, 348);
-            addActor.Margin = new Padding(4, 5, 4, 5);
+            addActor.Location = new Point(474, 209);
             addActor.Name = "addActor";
-            addActor.Size = new Size(107, 38);
+            addActor.Size = new Size(75, 23);
             addActor.TabIndex = 43;
             addActor.Text = "Add Actor";
             addActor.UseVisualStyleBackColor = true;
@@ -81,10 +78,9 @@
             // 
             // button1
             // 
-            button1.Location = new Point(677, 397);
-            button1.Margin = new Padding(4, 5, 4, 5);
+            button1.Location = new Point(474, 238);
             button1.Name = "button1";
-            button1.Size = new Size(107, 38);
+            button1.Size = new Size(75, 23);
             button1.TabIndex = 42;
             button1.Text = "Close";
             button1.UseVisualStyleBackColor = true;
@@ -93,122 +89,125 @@
             // movieFee
             // 
             movieFee.AutoSize = true;
-            movieFee.Location = new Point(220, 203);
-            movieFee.Margin = new Padding(4, 0, 4, 0);
+            movieFee.Location = new Point(154, 122);
             movieFee.Name = "movieFee";
-            movieFee.Size = new Size(39, 25);
+            movieFee.Size = new Size(25, 15);
             movieFee.TabIndex = 41;
             movieFee.Text = "Fee";
             // 
             // lbCopy
             // 
             lbCopy.AutoSize = true;
-            lbCopy.Location = new Point(220, 255);
-            lbCopy.Margin = new Padding(4, 0, 4, 0);
+            lbCopy.Location = new Point(154, 153);
             lbCopy.Name = "lbCopy";
-            lbCopy.Size = new Size(54, 25);
+            lbCopy.Size = new Size(35, 15);
             lbCopy.TabIndex = 40;
             lbCopy.Text = "Copy";
             // 
             // lbName
             // 
             lbName.AutoSize = true;
-            lbName.Location = new Point(220, 98);
-            lbName.Margin = new Padding(4, 0, 4, 0);
+            lbName.Location = new Point(154, 59);
             lbName.Name = "lbName";
-            lbName.Size = new Size(59, 25);
+            lbName.Size = new Size(39, 15);
             lbName.TabIndex = 39;
             lbName.Text = "Name";
             // 
             // lbType
             // 
             lbType.AutoSize = true;
-            lbType.Location = new Point(220, 150);
-            lbType.Margin = new Padding(4, 0, 4, 0);
+            lbType.Location = new Point(154, 90);
             lbType.Name = "lbType";
-            lbType.Size = new Size(49, 25);
+            lbType.Size = new Size(32, 15);
             lbType.TabIndex = 38;
             lbType.Text = "Type";
-            // 
-            // lbID
-            // 
-            lbID.AutoSize = true;
-            lbID.Location = new Point(220, 52);
-            lbID.Margin = new Padding(4, 0, 4, 0);
-            lbID.Name = "lbID";
-            lbID.Size = new Size(30, 25);
-            lbID.TabIndex = 37;
-            lbID.Text = "ID";
             // 
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(347, 65);
-            dataGridView1.Margin = new Padding(4, 5, 4, 5);
+            dataGridView1.Location = new Point(243, 39);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 62;
-            dataGridView1.Size = new Size(437, 250);
+            dataGridView1.Size = new Size(306, 150);
             dataGridView1.TabIndex = 36;
             // 
             // lbNumOfCpy
             // 
             lbNumOfCpy.AutoSize = true;
-            lbNumOfCpy.Location = new Point(12, 255);
+            lbNumOfCpy.Location = new Point(8, 153);
+            lbNumOfCpy.Margin = new Padding(2, 0, 2, 0);
             lbNumOfCpy.Name = "lbNumOfCpy";
-            lbNumOfCpy.Size = new Size(146, 25);
+            lbNumOfCpy.Size = new Size(96, 15);
             lbNumOfCpy.TabIndex = 35;
             lbNumOfCpy.Text = "Number of Copy";
             // 
             // lbFee
             // 
             lbFee.AutoSize = true;
-            lbFee.Location = new Point(12, 213);
+            lbFee.Location = new Point(8, 128);
+            lbFee.Margin = new Padding(2, 0, 2, 0);
             lbFee.Name = "lbFee";
-            lbFee.Size = new Size(39, 25);
+            lbFee.Size = new Size(25, 15);
             lbFee.TabIndex = 34;
             lbFee.Text = "Fee";
             // 
             // lbMovieType
             // 
             lbMovieType.AutoSize = true;
-            lbMovieType.Location = new Point(12, 163);
+            lbMovieType.Location = new Point(8, 98);
+            lbMovieType.Margin = new Padding(2, 0, 2, 0);
             lbMovieType.Name = "lbMovieType";
-            lbMovieType.Size = new Size(103, 25);
+            lbMovieType.Size = new Size(68, 15);
             lbMovieType.TabIndex = 33;
             lbMovieType.Text = "Movie Type";
             // 
             // lbMovieName
             // 
             lbMovieName.AutoSize = true;
-            lbMovieName.Location = new Point(12, 112);
+            lbMovieName.Location = new Point(8, 67);
+            lbMovieName.Margin = new Padding(2, 0, 2, 0);
             lbMovieName.Name = "lbMovieName";
-            lbMovieName.Size = new Size(113, 25);
+            lbMovieName.Size = new Size(75, 15);
             lbMovieName.TabIndex = 32;
             lbMovieName.Text = "Movie Name";
-            // 
-            // lbMovieID
-            // 
-            lbMovieID.AutoSize = true;
-            lbMovieID.Location = new Point(12, 65);
-            lbMovieID.Name = "lbMovieID";
-            lbMovieID.Size = new Size(84, 25);
-            lbMovieID.TabIndex = 31;
-            lbMovieID.Text = "Movie ID";
             // 
             // lbTitle
             // 
             lbTitle.AutoSize = true;
-            lbTitle.Location = new Point(12, 25);
+            lbTitle.Location = new Point(8, 15);
+            lbTitle.Margin = new Padding(2, 0, 2, 0);
             lbTitle.Name = "lbTitle";
-            lbTitle.Size = new Size(159, 25);
+            lbTitle.Size = new Size(105, 15);
             lbTitle.TabIndex = 30;
             lbTitle.Text = "View movie details";
             // 
+            // lbRate
+            // 
+            lbRate.AutoSize = true;
+            lbRate.Location = new Point(8, 184);
+            lbRate.Margin = new Padding(2, 0, 2, 0);
+            lbRate.Name = "lbRate";
+            lbRate.Size = new Size(87, 15);
+            lbRate.TabIndex = 46;
+            lbRate.Text = "Average Rating";
+            // 
+            // lbRating
+            // 
+            lbRating.AutoSize = true;
+            lbRating.Location = new Point(154, 184);
+            lbRating.Margin = new Padding(2, 0, 2, 0);
+            lbRating.Name = "lbRating";
+            lbRating.Size = new Size(87, 15);
+            lbRating.TabIndex = 47;
+            lbRating.Text = "Average Rating";
+            // 
             // ViewDetails
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(560, 270);
+            Controls.Add(lbRating);
+            Controls.Add(lbRate);
             Controls.Add(refreshButton);
             Controls.Add(lbActors);
             Controls.Add(addActor);
@@ -217,14 +216,13 @@
             Controls.Add(lbCopy);
             Controls.Add(lbName);
             Controls.Add(lbType);
-            Controls.Add(lbID);
             Controls.Add(dataGridView1);
             Controls.Add(lbNumOfCpy);
             Controls.Add(lbFee);
             Controls.Add(lbMovieType);
             Controls.Add(lbMovieName);
-            Controls.Add(lbMovieID);
             Controls.Add(lbTitle);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "ViewDetails";
             Text = "ViewDetails";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
@@ -242,13 +240,13 @@
         private Label lbCopy;
         private Label lbName;
         private Label lbType;
-        private Label lbID;
         private DataGridView dataGridView1;
         private Label lbNumOfCpy;
         private Label lbFee;
         private Label lbMovieType;
         private Label lbMovieName;
-        private Label lbMovieID;
         private Label lbTitle;
+        private Label lbRate;
+        private Label lbRating;
     }
 }
