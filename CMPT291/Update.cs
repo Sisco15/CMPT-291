f4bb262d2e42a1a18110152f46ee474a7abd9c19
zhirep <164239262+zhirep@users.noreply.github.com>
2025-12-02 20:14:37 -0700

    Update Update.cs

--- CMPT291/Update.cs
+++ CMPT291/Update.cs
@@ -21,7 +21,7 @@ namespace CMPT291
         public SqlDataReader myReader;
         private int movieID;
 
-        public Update(int MovieID, string name, string type, string fee, string copies)
+        public Update(int MovieID, string name, string type, string fee, string copies, string rating)
         {
             InitializeComponent();
 
@@ -37,7 +37,7 @@ namespace CMPT291
                 myCommand = new SqlCommand();
                 myCommand.Connection = connection; // Link the command stream to the connection
 
-                loadDetails(MovieID, name, type, fee, copies);
+                loadDetails(MovieID, name, type, fee, copies, rating);
 
             }
             catch (Exception e)
@@ -47,13 +47,14 @@ namespace CMPT291
             }
         }
 
-        private void loadDetails(int MovieID, string name, string type, string fee, string copies)
+        private void loadDetails(int MovieID, string name, string type, string fee, string copies, string rating)
         {
             movieID = MovieID;
             tbName.Text = name;
             tbType.Text = type;
             tbFee.Text = fee;
             tbCopy.Text = copies;
+            tbRate.Text = rating;
         }
 
         private void button1_Click_1(object sender, EventArgs e)
@@ -69,12 +70,14 @@ namespace CMPT291
                     "MovieType = '" + tbType.Text + "', " +
                     "Fee = " + tbFee.Text + ", " +
                     "NumOfCopy = " + tbCopy.Text + " " +
+                    "Movie Rating = " + tbRate.Text + " " +
                     "WHERE MovieID = " + movieID;
 
                 myCommand.Parameters.AddWithValue("@MovieName", tbName.Text);
                 myCommand.Parameters.AddWithValue("@MovieType", tbType.Text);
                 myCommand.Parameters.AddWithValue("@Fee", tbFee.Text);
                 myCommand.Parameters.AddWithValue("@NumOfCopy", tbCopy.Text);
+                myCommand.Parameters.AddWithValue("@MovieAveRate", tbRate.Text);
 
                 MessageBox.Show(myCommand.CommandText);
                 myCommand.ExecuteNonQuery();
@@ -88,4 +91,5 @@ namespace CMPT291
         }
     }
 }
+
 
