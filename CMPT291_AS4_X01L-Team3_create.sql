a35eca1cfaf4b88d231b91d744b9bc4432bc0984
zhirep <164239262+zhirep@users.noreply.github.com>
2025-12-03 11:46:40 -0700

    Update CMPT291_AS4_X01L-Team3_create.sql

--- CMPT291_AS4_X01L-Team3_create.sql
+++ CMPT291_AS4_X01L-Team3_create.sql
@@ -1,139 +1,162 @@
-Use Assignment4
+USE CMPT291_proj;
 
-Drop SEQUENCE IF EXISTS Customer_AccountNumber_Seq;
-DROP SEQUENCE IF EXISTS Movie_MovieID_Seq;
-DROP SEQUENCE IF EXISTS OrderID_Seq;
+--
+-- Clean up DB schema
+--
 
-DROP TABLE IF EXISTS AppearedIn;
+DROP SEQUENCE IF EXISTS Customer_CustomerID_Seq;
+DROP SEQUENCE IF EXISTS Employee_EmployeeID_Seq;
+
+DROP TABLE IF EXISTS ActorRate;
+DROP TABLE IF EXISTS RentalRecord;
+DROP TABLE IF EXISTS ActorAppear;
 DROP TABLE IF EXISTS Actor;
+DROP TABLE IF EXISTS CustomerQueue;
+DROP TABLE IF EXISTS Movie;
 DROP TABLE IF EXISTS CustomerPhone;
 DROP TABLE IF EXISTS Customer;
 DROP TABLE IF EXISTS EmployeePhone;
 DROP TABLE IF EXISTS Employee;
-DROP TABLE IF EXISTS Movie;
-DROP TABLE IF EXISTS QueueUp;
-DROP TABLE IF EXISTS Order_Req;
-DROP TABLE IF EXISTS Rate_Actor;
+DROP TABLE IF EXISTS User_log;
 
-CREATE TABLE Order_Req (
-OrderID INT NOT NULL,
-Checkout_Date DATE NOT NULL  DEFAULT(getdate()),
-Return_Date DATE NULL,
-Cust_rate_movie INT NULL,
-MID INT NOT NULL,
-CID INT NOT NULL,
-EID INT NOT NULL,
-PRIMARY KEY( OrderID ),
-FOREIGN KEY (MID) REFERENCES Movie(MovieID),
-FOREIGN KEY (CID) REFERENCES Customer(CustomerID),
-FOREIGN KEY (EID) REFERENCES Employee(EmployeeID),
-);
-CREATE SEQUENCE OrderID_Seq start with 1000 INCREMENT by 1;
+--
+-- Create tables
+-- https://www.w3schools.com/sql/sql_create_db.asp
+-- https://learn.microsoft.com/en-us/sql/t-sql/data-types/data-types-transact-sql?view=sql-server-ver16
+-- https://learn.microsoft.com/en-us/sql/relational-databases/tables/unique-constraints-and-check-constraints?view=sql-server-ver16
+--
 
-CREATE TABLE Rate_Actor (
-AID INT NOT NULL,
-OrdID INT NOT NULL,
-ARate INT NOT NULL,
-PRIMARY KEY (AID, OrdID),
-FOREIGN KEY (OrdID) REFERENCES Order_Req(OrderID),
-FOREIGN KEY (AID) REFERENCES Actor(ActorID)
+CREATE TABLE User_log(
+EmployeeID INT,
+username VARCHAR(40) NOT NULL,
+pass_word VARCHAR(40) NOT NULL,
+SSN NCHAR(9) NOT NULL,
+PRIMARY KEY( EmployeeID )
 );
 
 CREATE TABLE Employee (
-EmployeeID INT IDENTITY(1,1),
+EmployeeID INT,
 SSN NCHAR(9) NOT NULL,
 LastName VARCHAR(40) NOT NULL,
 FirstName VARCHAR(40) NOT NULL,
-Address VARCHAR(35) NOT NULL,
-City VARCHAR(50) NOT NULL,
-State VARCHAR (50) NOT NULL,
-ZipCode VARCHAR(10) NOT NULL,
-StartDate DATE NOT NULL DEFAULT(getdate()),
-PRIMARY KEY( EmployeeID ),
-UNIQUE ( SSN ),
+Address VARCHAR(40),
+City VARCHAR(40),
+Province NCHAR(2),
+PostalCode NCHAR(6),
+StartDate DATE NOT NULL,
+PRIMARY KEY( EmployeeID )
 );
+
+
+CREATE SEQUENCE Employee_EmployeeID_Seq START WITH 1000 INCREMENT BY 1;
+
 
 CREATE TABLE EmployeePhone (
 EmployeeID INT NOT NULL,
 PhoneNum NCHAR(10) NOT NULL,
 PhoneType VARCHAR(10) NOT NULL,
-PRIMARY KEY( EmployeeID, PhoneNum),
+StartTime DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+EndTime DATETIME,
+PRIMARY KEY( EmployeeID, PhoneNum, StartTime ),
 FOREIGN KEY ( EmployeeID ) REFERENCES Employee( EmployeeID ),
+CONSTRAINT EmpPhonePeriod CHECK (StartTime < EndTime)
 );
-
 
 
 CREATE TABLE Customer (
-CustomerID INT IDENTITY(1,1),
+CustomerID INT,
 LastName VARCHAR(40) NOT NULL,
 FirstName VARCHAR(40) NOT NULL,
-Address VARCHAR(35) NOT NULL,
-City VARCHAR(50) NOT NULL,
-State VARCHAR (50) NOT NULL,
-ZipCode VARCHAR(10) NOT NULL,
-AccountNumber INT NOT NULL,
-EmailAddress VARCHAR(50) NOT NULL,
-AccountCreateDate DATE NOT NULL DEFAULT(getdate()),
-CreditCardNumber VARCHAR(20) NOT NULL,
-PRIMARY KEY( CustomerID ),
-UNIQUE( AccountNumber ),
+Address VARCHAR(40),
+City VARCHAR(40),
+Province NCHAR(2),
+PostalCode NCHAR(6),
+Email VARCHAR(40) NOT NULL,
+AccountNum NCHAR(10) NOT NULL,
+CreditCardNum NCHAR(16) NOT NULL,
+CreditCardExp NCHAR(4) NOT NULL,
+CreditCardCvv NCHAR(3) NOT NULL,
+CreationDate DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY( CustomerID )
 );
-CREATE SEQUENCE Customer_AccountNumber_Seq start with 1000 INCREMENT by 1;
+
+CREATE SEQUENCE Customer_CustomerID_Seq START WITH 1000 INCREMENT BY 1;
 
 CREATE TABLE CustomerPhone (
 CustomerID INT NOT NULL,
 PhoneNum NCHAR(10) NOT NULL,
 PhoneType VARCHAR(10) NOT NULL,
-PRIMARY KEY( CustomerID, PhoneNum),
+StartTime DATETIME NOT NULL DEFAULT(getdate()),
+EndTime DATETIME,
+PRIMARY KEY( CustomerID, PhoneNum, StartTime ),
 FOREIGN KEY ( CustomerID ) REFERENCES Customer( CustomerID ),
-
+CONSTRAINT CustPhonePeriod CHECK (StartTime < EndTime)
 );
 
+
 CREATE TABLE Movie (
-MovieID INT NOT NULL,
+MovieID INT NOT NULL IDENTITY(1,1),
 MovieName VARCHAR(40) NOT NULL,
 MovieType VARCHAR(10) NOT NULL
-	CHECK( MovieType='Comedy' or MovieType='Drama' or MovieType='Action' or MovieType='Adventure' 
-	or MovieType='Scifi' or MovieType='Horror' or MovieType='Romance' or MovieType='Thriller'),
+	CHECK( MovieType='Comedy' or MovieType='Drama' or MovieType='Action' or MovieType='Foreign' ),
 Fee NUMERIC(6,2) NOT NULL,
 NumOfCopy INT NOT NULL,
+MovieAveRate INT NOT NULL,
 PRIMARY KEY( MovieID )
 );
 
-CREATE TABLE QueueUP (
-MID INT NOT NULL,
-CID INT NOT NULL,
-SortOrder INT NOT NULL,
-PRIMARY KEY (MID, CID),
-FOREIGN KEY (MID) REFERENCES Movie(MovieID),
-FOREIGN KEY (CID) REFERENCES Customer(CustomerID)
+
+CREATE TABLE CustomerQueue (
+CustomerID INT NOT NULL,
+MovieID INT NOT NULL,
+SortNum INT NOT NULL,
+PRIMARY KEY ( CustomerID, MovieID ),
+UNIQUE ( CustomerID, SortNum ),
+FOREIGN KEY ( CustomerID ) REFERENCES Customer( CustomerID ),
+FOREIGN KEY ( MovieID ) REFERENCES Movie( MovieID ) ON DELETE CASCADE
 );
+
+
 
 CREATE TABLE Actor (
 ActorID INT IDENTITY(1,1),
-ActorName VARCHAR(60) NOT NULL,
-Gender CHAR(1) CHECK (Gender IN ('M', 'F')),
-DoB DATE NOT NULL,
-PRIMARY KEY (ActorID)
+Name VARCHAR(100) NOT NULL,
+Gender NCHAR(1) NOT NULL CHECK( Gender='M' or Gender='F' ),
+DateOfBrith Date NOT NULL,
+PRIMARY KEY( ActorID )
 );
 
-CREATE TABLE AppearedIn (
-ActorID INT NOT NULL,
+CREATE TABLE ActorAppear (
 MovieID INT NOT NULL,
-PRIMARY KEY (ActorID, MovieID),
-FOREIGN KEY (ActorID) REFERENCES Actor(ActorID),
-FOREIGN KEY (MovieID) REFERENCES Movie(MovieID)
+ActorID INT NOT NULL,
+PRIMARY KEY ( MovieID, ActorID ),
+FOREIGN KEY ( MovieID ) REFERENCES Movie( MovieID ) ON DELETE CASCADE,
+FOREIGN KEY ( ActorID ) REFERENCES Actor( ActorID )
 );
 
-CREATE SEQUENCE Movie_MovieID_Seq START WITH 1000 INCREMENT BY 1;
 
 
+CREATE TABLE RentalRecord (
+RentalRecordID INT IDENTITY(1,1),
+EmployeeID INT NOT NULL,
+CustomerID INT NOT NULL,
+MovieID INT NOT NULL,
+CheckoutTime DATETIME NOT NULL DEFAULT(getdate()),
+ReturnTime DATETIME,
+MovieRate INT,
+PRIMARY KEY( RentalRecordID ),
+FOREIGN KEY ( EmployeeID ) REFERENCES Employee( EmployeeID ),
+FOREIGN KEY ( CustomerID ) REFERENCES Customer( CustomerID ),
+FOREIGN KEY ( MovieID ) REFERENCES Movie( MovieID ) ON DELETE CASCADE
+);
 
 
-
-
-
-
-
+CREATE TABLE ActorRate (
+RentalRecordID INT NOT NULL,
+ActorID INT NOT Null,
+ActorRate INT,
+FOREIGN KEY ( RentalRecordID ) REFERENCES RentalRecord( RentalRecordID ) ON DELETE CASCADE,
+FOREIGN KEY ( ActorID ) REFERENCES Actor( ActorID )
+);
 
 
